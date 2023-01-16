fe4b61516291651bec13a620f8a6585f3c131e6c
JE-02 <JEspinosa2022Tecet@gmail.com>
2023-01-16 15:16:02 -0300

    Task 5

---
+++ Task 5/exercise 1.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("PRODUCT --> PC $15000");
+Console.Write("NAME: ");
+string name = Console.ReadLine();
+Console.Write("EMAIL: ");
+string email = Console.ReadLine();
+Console.Write("DO YOU HAVE A CUPON Y/N: ");
+char doHaveCupon = Convert.ToChar(Console.ReadLine());
+bool cupon = (doHaveCupon == 'Y') ? true : (doHaveCupon == 'N') ? false : false;
+
+if (cupon)
+{
+    Console.Write("WHAT TYPE OF CUPON (10/20/30): ");
+    int cuponAmount = Convert.ToInt32(Console.ReadLine());
+
+    switch (cuponAmount)
+    {
+        case 10:
+            Console.WriteLine($"PRICE WITH 10% DISCOUNT: {15000 - (15000 * 0.1)} ");
+            break;
+        case 20:
+            Console.WriteLine($"PRICE WITH 20% DISCOUNT: {15000 - (15000 * 0.2)} ");
+            break;
+        case 30:
+            Console.WriteLine($"PRICE WITH 10% DISCOUNT: {15000 - (15000 * 0.3)} ");
+            break;
+        default:
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("PRICE: 15000");
+}
+
+
+
---
+++ Task 5/exercise 2.cs
@@ -0,0 +1,29 @@
+﻿string[] languagesList = { "C", "Javascript", "C#", "Python", "Pascal" };
+Console.WriteLine("Elige un lenguaje de programacion 1-C 2-Javascript 3-C# 4-Python 5-Pascal");
+int choice = Convert.ToInt32(Console.ReadLine());
+switch (choice)
+{
+    case 1:
+        break;
+        Console.WriteLine(languagesList[0]);
+
+    case 2:
+        Console.WriteLine(languagesList[1]);
+        break;
+
+    case 3:
+        Console.WriteLine(languagesList[2]);
+        break;
+
+    case 4:
+        Console.WriteLine(languagesList[3]);
+        break;
+
+    case 5:
+        Console.WriteLine(languagesList[4]);
+        break;
+
+    default:
+        Console.WriteLine("THAT OPTION DOESNT EXIST!");
+        break;
+}
